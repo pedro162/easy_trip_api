9fc1fcc1903d1ca025ea208b62f39c3e00651c70
Pedro <phedroclooney@gmail.com>
2024-05-22 21:04:07 -0300

    Fixing some bugs

--- app/Domain/TripDomain.php
+++ app/Domain/TripDomain.php
@@ -1,15 +1,17 @@
-<?php 
+<?php
 
 namespace App\Domain;
 
 use App\Validators\TripValidator;
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;
 use App\Exceptions\TripException;
 use App\Models\Trip;
 
-class TripDomain{
+class TripDomain
+{
 
-	public function index(){
+	public function index()
+	{
 
 		//----- Select active records ---------------------------------------
 		$result = Trip::all();
@@ -17,10 +19,11 @@ class TripDomain{
 		return $result;
 	}
 
-	public function create(array $data = []){
+	public function create(array $data = [])
+	{
 
 		//----- Select only the necessary informations ---------------------------
-		
+
 		$dataToTrip = $dataToTrip = [
 			'driver_id'				=> $data['driver_id'] 			?? null,
 			'client_id'				=> $data['client_id'] 			?? null,
@@ -38,14 +41,14 @@ class TripDomain{
 
 		//----- Validate infomations ----------------------------------------------
 		$erros = TripValidator::validateDataToCreateTrip($dataToTrip, new Trip());
-		if(is_array($erros) && count($erros) > 0){
+		if (is_array($erros) && count($erros) > 0) {
 
 			$strErros = implode(', ', $erros);
 			throw new TripException($strErros);
 		}
-		
+
 		$tripObject = Trip::create($dataToTrip);
-		if(! $tripObject){
+		if (!$tripObject) {
 			throw new UserException("Something went wrong. It was not possible to create a new trip. Please try again or contact support.");
 		}
 
@@ -53,29 +56,31 @@ class TripDomain{
 	}
 
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id){
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(string $id)
+	{
 
-    	$id = (int) $id;
-    	if(!($id > 0)){
+		$id = (int) $id;
+		if (!($id > 0)) {
 			$strErro = "The record code informaded isn't valid {$id}";
 			throw new TripException($strErro);
 		}
 
-        //----- Try to load the record -------------------------------------------
+		//----- Try to load the record -------------------------------------------
 
 		$tripObject = Trip::find($id);
-		if(!$tripObject){
+		if (!$tripObject) {
 			//$strErro = "It was not possible to locale the record of code number {$id}";
 			///throw new TripException($strErro);
 		}
 
 		return $tripObject;
-    }
+	}
 
-    protected function buildDataToStoreTrip(array $data = [], Trip $tripObject = null){
+	protected function buildDataToStoreTrip(array $data = [], Trip $tripObject = null)
+	{
 		$dataToTrip = [
 			'driver_id'				=> $data['driver_id'] 			?? $tripObject->driver_id,
 			'client_id'				=> $data['client_id'] 			?? $tripObject->client_id,
@@ -96,24 +101,25 @@ class TripDomain{
 
 
 	/**
-     * Update the specified resource in storage.
-     */
-	public function update(string $id , array $data = []){
+	 * Update the specified resource in storage.
+	 */
+	public function update(string $id, array $data = [])
+	{
 		$id = (int) $id;
-		if(!($id > 0)){
+		if (!($id > 0)) {
 			$strErro = "The record code informaded isn't valid {$id}";
 			throw new TripException($strErro);
 		}
 
 		//----- Try to load the record -------------------------------------------
-		$tripObject = Trip::find($id);		
-		if(!$tripObject){
+		$tripObject = Trip::find($id);
+		if (!$tripObject) {
 			$strErro = "It was not possible to locale the record of code number {$id}";
 			throw new TripException($strErro);
 		}
 
-		$loggedUserObj = \Auth::User(); 
-		if(!in_array($loggedUserObj->user_type, ['customer'])){
+		$loggedUserObj = \Auth::User();
+		if (!in_array($loggedUserObj->user_type, ['customer'])) {
 			$strErro = "You cannot change the trip informations of code number {$id}";
 			throw new TripException($strErro);
 		}
@@ -130,8 +136,8 @@ class TripDomain{
 		];
 		//----- Validate infomations ----------------------------------------------
 		$erros = TripValidator::validateDataToCreateTrip($dataToTrip);
-		if(is_array($erros) && count($erros) > 0){
-			
+		if (is_array($erros) && count($erros) > 0) {
+
 			$strErros = implode(', ', $erros);
 			throw new TripException($strErros);
 		}
@@ -143,27 +149,27 @@ class TripDomain{
 	}
 
 	/**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
+	 * Remove the specified resource from storage.
+	 */
+	public function destroy(string $id)
+	{
 
-    	$id = (int) $id;
+		$id = (int) $id;
 
-		if(!($id > 0)){
+		if (!($id > 0)) {
 			$strErro = "The record code informad isn't valid {$id}";
 			throw new TripException($strErro);
 		}
 
-    	//----- Try to load the record -------------------------------------------
-        $tripObject = Trip::find($id);
-		if(!$tripObject){
+		//----- Try to load the record -------------------------------------------
+		$tripObject = Trip::find($id);
+		if (!$tripObject) {
 			$strErro = "It was not possible to locale the trip of code number {$id}";
 			throw new TripException($strErro);
 		}
-		
-		if(!in_array($tripObject->trip_state, ['waiting', 'canceled'])){
-			$strErro = "It was not possible to cancel the trip with code number {$id}, because it's {$tripObject->trip_state}";
+
+		if (!in_array($tripObject->trip_state, ['waiting', 'canceled'])) {
+			$strErro = "It was not possible to delete the trip with code number {$id}, because it's {$tripObject->trip_state}";
 			throw new TripException($strErro);
 		}
 
@@ -171,87 +177,90 @@ class TripDomain{
 		$tripObject->delete();
 
 		return true;
-    }
+	}
 
-    public function startTrip(string $id, array $data=[]){
+	public function startTrip(string $id, array $data = [])
+	{
 
-    	//----- Try to load the trip record -------------------------------------------
-        $tripObject = Trip::find($id);
-		if(!$tripObject){
+		//----- Try to load the trip record -------------------------------------------
+		$tripObject = Trip::find($id);
+		if (!$tripObject) {
 			$strErro = "It was not possible to locale the trip of code number {$id}";
 			throw new TripException($strErro);
 		}
 
-		$loggedUserObj = \Auth::User(); 
-		if(!in_array($loggedUserObj->user_type, ['driver'])){
+		$loggedUserObj = \Auth::User();
+		if (!in_array($loggedUserObj->user_type, ['driver'])) {
 			$strErro = "You cannot start the trip of code number {$id}";
 			throw new TripException($strErro);
 		}
 
-		if(!in_array($tripObject->trip_state, ['waiting'])){
-			$strErro = "It was not possible to cancel the trip with code number {$id}, because it's {$tripObject->trip_state}";
+		if (!in_array($tripObject->trip_state, ['waiting'])) {
+			$strErro = "It was not possible to start the trip with code number {$id}, because it's {$tripObject->trip_state}";
 			throw new TripException($strErro);
 		}
 
 		//----- Update to load the record -------------------------------------------
 		$dataToTrip = [
-			'trip_state'=>'started',
-			'driver_id'=>$loggedUserObj->id
+			'trip_state' => 'started',
+			'driver_id' => $loggedUserObj->id
 		];
 		$tripObject->update($dataToTrip);
-		
+
 		return $tripObject;
-    }
+	}
 
-    public function cancelTrip(string $id, array $data=[]){
+	public function cancelTrip(string $id, array $data = [])
+	{
 
-    	//----- Try to load the trip record -------------------------------------------
-        $tripObject = Trip::find($id);
-		if(!$tripObject){
-			$strErro = "It was not possible to locale the trip of code number {$id}";
-			throw new TripException($strErro);
-		}
-		
-		if(!in_array($tripObject->trip_state, ['waiting'])){
-			$strErro = "It was not possible to cancel the trip with code number {$id}, because it's {$tripObject->trip_state}";
-			throw new TripException($strErro);
-		}
-
-		//----- Update to load the record -------------------------------------------
-		$dataToTrip = [
-			'trip_state'=>'canceled'
-		];
-		$tripObject->update($dataToTrip);
-		
-		return $tripObject;
-    }
-
-    public function compliteTrip(string $id, array $data=[]){
-
-    	//----- Try to load the trip record -------------------------------------------
-        $tripObject = Trip::find($id);
-		if(!$tripObject){
+		//----- Try to load the trip record -------------------------------------------
+		$tripObject = Trip::find($id);
+		if (!$tripObject) {
 			$strErro = "It was not possible to locale the trip of code number {$id}";
 			throw new TripException($strErro);
 		}
 
-		$loggedUserObj = \Auth::User(); 
-		if(!in_array($loggedUserObj->user_type, ['driver'])){
-			$strErro = "You cannot start the trip of code number {$id}";
-			throw new TripException($strErro);
-		}
-
-		if(!in_array($tripObject->trip_state, ['started'])){
+		if (!in_array($tripObject->trip_state, ['waiting'])) {
 			$strErro = "It was not possible to cancel the trip with code number {$id}, because it's {$tripObject->trip_state}";
 			throw new TripException($strErro);
 		}
 
 		//----- Update to load the record -------------------------------------------
 		$dataToTrip = [
-			'trip_state'=>'finished'
+			'trip_state' => 'canceled'
 		];
 		$tripObject->update($dataToTrip);
-		
+
 		return $tripObject;
-    } 
+	}
+
+	public function compliteTrip(string $id, array $data = [])
+	{
+
+		//----- Try to load the trip record -------------------------------------------
+		$tripObject = Trip::find($id);
+		if (!$tripObject) {
+			$strErro = "It was not possible to locale the trip of code number {$id}";
+			throw new TripException($strErro);
+		}
+
+		$loggedUserObj = \Auth::User();
+		if (!in_array($loggedUserObj->user_type, ['driver'])) {
+			$strErro = "You cannot complete the trip of code number {$id}";
+			throw new TripException($strErro);
+		}
+
+		if (!in_array($tripObject->trip_state, ['started'])) {
+			$strErro = "It was not possible to complete the trip with code number {$id}, because it's {$tripObject->trip_state}";
+			throw new TripException($strErro);
+		}
+
+		//----- Update to load the record -------------------------------------------
+		$dataToTrip = [
+			'trip_state' => 'finished'
+		];
+		$tripObject->update($dataToTrip);
+
+		return $tripObject;
+	}
 }
